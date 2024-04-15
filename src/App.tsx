f470b52f35f2fb724f6556effa1e3f1ec9cfc177
sohho888 <sohho888@email.ru>
2024-04-15 21:58:21 +0100

    create url client function

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import styles from './App.module.scss';
 import targetTime from './utils';
 
 function App() {
-  const urlCurrent = window.location.href;
-  const clientTtimeStamp = new Date().getTime();
-  const clientTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const urlClient = `${urlCurrent}/?ts=${clientTtimeStamp}&tz=${clientTimeZone}`;
-
-  console.log(urlClient);
+  const getClientUrl = () => {
+    const urlCurrent = window.location.href;
+    const clientTtimeStamp = new Date().getTime();
+    const clientTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const urlClient = `${urlCurrent}/?ts=${clientTtimeStamp}&tz=${clientTimeZone}`;
+    return urlClient;
+  };
 
   // console.log(`Ссылка: ${urlCurrent}/?ts=${clientTtimeStamp}tz=${clientTimeZone} `);
   // const url = 'https://timesync.club/?ts=1712745573031&tz=Asia/Tokyo';
@@ -20,7 +21,7 @@ function App() {
           Secret meeting of the <span> Masons</span>
         </h1>
         <div className={styles.block}>
-          <Clock time={new Date(targetTime(urlClient))} />
+          <Clock time={new Date(targetTime(getClientUrl()))} />
           <Clock time={new Date()} />
         </div>
       </div>
